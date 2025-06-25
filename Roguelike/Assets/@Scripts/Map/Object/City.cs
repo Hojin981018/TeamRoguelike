7d5373b19ed76cffd28f4811d2fb260c80057e52
Hojin981018 <mhj981018@gmail.com>
2025-06-25 13:30:16 +0900

    Revert "0.0.5.1"

--- Roguelike/Assets/@Scripts/Map/Object/City.cs
+++ Roguelike/Assets/@Scripts/Map/Object/City.cs
@@ -1,52 +1,16 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class City : MonoBehaviour
 {
-    
-    public Transform[] bd;
-    
-
-
-
-    float h;
-    public float speed;
-
-
-
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { 
-        speed = 0.03f;      
+    {
+        
     }
+
+    // Update is called once per frame
     void Update()
     {
-        Move();
-    }
-
-    void Move()
-    {
-        if (Input.GetButton(Define.Horizontal))
-        {
-            h = Input.GetAxisRaw(Define.Horizontal);
-            Vector2 moveDir = new Vector2(-h, 0);
-            
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-
-                for (int i = 0; i < bd.Length ; i++) 
-                {
-                    float a = 0.8f * (i + 1);
-                    bd[i].transform.Translate(Time.deltaTime * speed * a * moveDir);
-                }
-            }
-            else
-            {
-                for (int i = 0; i < bd.Length; i++)
-                {
-                    float a =  0.4f * (i + 1);
-                    bd[i].transform.Translate(Time.deltaTime * speed * a * moveDir);
-                }
-            }
-        }
+        
     }
 }
